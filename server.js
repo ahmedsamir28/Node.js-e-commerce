cc953dfc834a086b5a31751e83f31b72b589bf4a
Ahmed Samir <155876773+ahmedsamir28@users.noreply.github.com>
2024-06-05 02:20:30 +0300

    connect the project with database

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-const express = require("express")
-const dotenv = require("dotenv")
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 dotenv.config({path: 'config.env'})
+
 const app = express()
 
+// connect with db
+mongoose.connect(process.env.DB_URI).then((conn)=>{
+    console.log(`Database Connect: ${conn.connection.host}`);
+}).catch((err)=>{
+    console.error(`Data Error: ${err}`);
+    process.exit(1); //Stop the node.js application
+})
+
+
+if (process.env.NODE_ENV ==='development') {
+    app.use(morgan('dev'))
+    console.log(`mode: ${process.env.NODE_ENV}`);
+}
 
 app.get('/',(req,res,next)=> {
-    res.send('ahmed sarmi')
+    res.send('ahmed samir')
 })
 
 
