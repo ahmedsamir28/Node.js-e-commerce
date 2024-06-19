9f5433886a6b06eb18a41be6d5f197ebb6fe0981
Ahmed Samir <155876773+ahmedsamir28@users.noreply.github.com>
2024-06-19 12:07:20 +0300

    get specific  category

--- Routes/categoryRoute.js
+++ Routes/categoryRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { getCategories, createCategory } = require('../Services/categoryService')
+const { getCategories, createCategory, getCategory } = require('../Services/categoryService')
 
 const router = express.Router()
 router.route('/').get(getCategories).post(createCategory)
+router.route('/:id').get(getCategory)
 
 
 module.exports = router;
--- Services/categoryService.js
+++ Services/categoryService.js
@@ -2,15 +2,26 @@ const categoryModel = require('../Models/categoryModel')
 const slugify = require('slugify')
 const asyncHandler = require('express-async-handler')
 
-//het list of categories
-exports.getCategories =asyncHandler(async(req, res) => {
-    const page = req.query.page * 1 || 1 
+//get list of categories
+exports.getCategories = asyncHandler(async (req, res) => {
+    const page = req.query.page * 1 || 1
     const limit = req.query.limit * 1 || 5
     const skip = (page - 1) * limit
     const categories = await categoryModel.find({}).skip(skip).limit(limit)
-    res.status(200).json({results:categories.length , data:categories })
+    res.status(200).json({ results: categories.length, data: categories })
 
 })
+
+//get specific category 
+exports.getCategory = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const category = await categoryModel.findById(id)
+    if (!category) {
+        res.status(404).json({ msg: `No category for this id ${id}` })
+    }
+    res.status(200).json({ data: category })
+}
+)
 
 {
 // use async and await
